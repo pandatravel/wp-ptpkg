6830c636d508a9ff60bc36272bce272f08abac8d
Ammon Casey <ammon@caseyohana.com>
2017-12-27 15:06:53 -1000

    Initial Commit

---
+++ includes/class-ptpkg-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://www.pandaonline.com
+ * @since      1.0.0
+ *
+ * @package    Ptpkg
+ * @subpackage Ptpkg/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Ptpkg
+ * @subpackage Ptpkg/includes
+ * @author     Ammon Casey <acasey@panda-group.com>
+ */
+class Ptpkg_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'ptpkg',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
