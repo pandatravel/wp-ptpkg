01d1abada9efc945609606a6241967e6f2cb2d0c
Ammon Casey <ammon@caseyohana.com>
2018-02-05 13:00:31 -1000

    [mix] refactor styles build process to use bootstrap glyphicons

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('assets/src/admin/js/admin.js', './assets/admin/js/ptpkg-admin.js')
-   .sass('assets/src/admin/sass/admin.scss', './assets/admin/css/ptpkg-admin.css')
-   .js('assets/src/public/js/public.js', './assets/public/js/ptpkg-public.js')
-   .sass('assets/src/public/sass/public.scss', './assets/public/css/ptpkg-public.css');
+ // put fonts in assetDir
+ mix.config.publicPath = 'assets'
+ mix.config.fileLoaderDirs.fonts = 'public/fonts';
+
+mix.js('assets/src/admin/js/admin.js', 'admin/js/ptpkg-admin.js')
+   .sass('assets/src/admin/sass/admin.scss', 'admin/css/ptpkg-admin.css')
+   .js('assets/src/public/js/app.js', 'public/js/ptpkg-app.js')
+   .js('assets/src/public/js/public.js', 'public/js/ptpkg-public.js')
+   .sass('assets/src/public/sass/public.scss', 'public/css/ptpkg-public.css');
