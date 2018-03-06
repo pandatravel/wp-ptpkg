90c89a1976cdd39e2d7d9bc429ca5a9e1665ae8b
Ammon Casey <ammon@caseyohana.com>
2018-03-06 10:54:01 -1000

    [js] credit card list mixin

---
+++ assets/src/public/js/mixins/creditCards.js
@@ -0,0 +1,16 @@
+const creditCards = {
+
+    data() {
+        return {
+            creditCards: [
+                {"card": "visa", "name": "Visa"},
+                {"card": "mastercard", "name": "Mastercard"},
+                {"card": "amex", "name": "American Express"},
+                {"card": "discover", "name": "Discover Card"},
+                {"card": "jcb", "name": "JCB"},
+            ]
+        }
+    },
+};
+
+export default creditCards;
