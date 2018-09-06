993e100097ebe9aa0181827f91b8d5c96745a7b3
Ammon Casey <ammon@caseyohana.com>
2018-09-06 09:55:57 -1000

    [tpl|scss] Fixes layout bugs with the new versionFour theme

--- templates/single-package-book.php
+++ templates/single-package-book.php
@@ -24,28 +24,32 @@ $packagePrice = get_post_meta($currentID, 'package-price', true);
 get_header(); ?>
 
 <!-- Start single-package-book.php -->
-<div id="page-body" class="package-single-wrapper">
+<div id="page-body" class="package-single-wrapper pt-3">
     <div id="app" class="package-book" v-cloak><!-- vue root -->
         <notifications group="package" position="bottom right" :duration="4000"></notifications>
 
         <v-app>
+            <v-content class="page-content">
+                <v-container class="page-content-wrapper" pa-0>
 
-            <corner-ribbon color="danger"></corner-ribbon>
-            <v-jumbotron src="<?php echo $packageBannerUrl; ?>" height="333px" class="package-banner mb-3">
-                <v-container fill-height py-5>
-                    <v-layout row pt-5>
-                        <v-flex sm9 offset-sm3 text-sm-left>
-                            <h2 class="package-title white--text"><?php the_title(); ?></h2>
-                            <div class="package-teaser">
-                                <?php echo $packageTeaser; ?>
-                            </div>
+                    <v-layout align-start justify-center row class="hidden-sm-and-down">
+                        <v-flex xs12 py-0>
+                            <corner-ribbon color="danger"></corner-ribbon>
+                            <v-jumbotron src="<?php echo $packageBannerUrl; ?>" height="300px" class="package-banner">
+                                <v-container fill-height py-5>
+                                    <v-layout row pt-5>
+                                        <v-flex sm9 offset-sm3 text-sm-left>
+                                            <h2 class="package-title white--text"><?php the_title(); ?></h2>
+                                            <div class="package-teaser">
+                                                <?php echo $packageTeaser; ?>
+                                            </div>
+                                        </v-flex>
+                                    </v-layout>
+                                </v-container>
+                            </v-jumbotron>
                         </v-flex>
                     </v-layout>
-                </v-container>
-            </v-jumbotron>
 
-            <v-content>
-                <v-container fluid>
                     <?php
                     /* Start the Loop */
                     while (have_posts()) : the_post();
@@ -56,7 +60,6 @@ get_header(); ?>
                     ?>
                 </v-container>
             </v-content>
-
         </v-app>
 
     </div><!-- #app -->
--- templates/single-package.php
+++ templates/single-package.php
@@ -18,11 +18,11 @@ $templates = new Ptpkg\lib\common\CustomTemplateLoader;
  */
 $currentID = get_the_ID();
 $packageBannerUrl = wp_get_attachment_image_url(get_post_meta($currentID, 'package-banner', true), 'full');
-$packageTeaser = get_post_meta($currentID, 'package-teaser', true);
 $packagePrice = get_post_meta($currentID, 'package-price', true);
 $packageLocation = get_post_meta(get_the_ID(), 'package-location', true);
-
-setPostViews(get_the_ID());
+$packageTeaser = get_post_meta($currentID, 'package-teaser', true);
+$packageSEOAd = get_post_meta($currentID, 'package-seo-ad', true);
+$packageSEOContent = get_post_meta($currentID, 'package-seo-content', true);
 
 get_header(); ?>
 
@@ -43,30 +43,35 @@ get_header(); ?>
 </script>
 
 <!-- Start single-package.php -->
-<div id="page-body" class="package-single-wrapper">
+<div id="page-body" class="package-single-wrapper pt-3">
     <div id="app" class="package-details" v-cloak><!-- vue root -->
 
         <v-app>
 
-            <corner-ribbon color="danger"></corner-ribbon>
-            <v-jumbotron src="<?php echo $packageBannerUrl; ?>" height="333px" class="package-banner mb-3">
-                <v-container fill-height py-5>
-                    <v-layout row wrap pt-5>
-                        <v-flex sm4 offset-sm4 class="package-cta-book">
-                            <action-button :id="<?php echo $currentID ?>" url="<?php echo get_post_permalink($currentID) ?>" color="primary" large block round load-status></action-button>
-                        </v-flex>
-                        <v-flex sm9 offset-sm3 text-sm-left>
-                            <h2 class="package-title white--text"><?php the_title(); ?></h2>
-                            <div class="package-teaser">
-                                <?php echo $packageTeaser; ?>
-                            </div>
+            <v-content class="page-content">
+                <v-container class="page-content-wrapper" pa-0>
+
+                    <v-layout align-start justify-center class="hidden-sm-and-down">
+                        <v-flex xs12 py-0>
+                            <corner-ribbon color="danger"></corner-ribbon>
+                            <v-jumbotron src="<?php echo $packageBannerUrl; ?>" height="300px" class="package-banner mb-3">
+                                <v-container fill-height py-5>
+                                    <v-layout row wrap pt-5>
+                                        <v-flex sm4 offset-sm4 class="package-cta-book">
+                                            <action-button :id="<?php echo $currentID ?>" url="<?php echo get_post_permalink($currentID) ?>" color="primary" large block round load-status></action-button>
+                                        </v-flex>
+                                        <v-flex sm9 offset-sm3 text-sm-left>
+                                            <h2 class="package-title white--text"><?php the_title(); ?></h2>
+                                            <div class="package-teaser">
+                                                <?php echo $packageTeaser; ?>
+                                            </div>
+                                        </v-flex>
+                                    </v-layout>
+                                </v-container>
+                            </v-jumbotron>
                         </v-flex>
                     </v-layout>
-                </v-container>
-            </v-jumbotron>
 
-            <v-content>
-                <v-container fluid>
                     <?php
                     /* Start the Loop */
                     while (have_posts()) : the_post();
