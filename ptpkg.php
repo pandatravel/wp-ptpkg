5fd62bbb12375a42c3b32ae71abdc7e755e329f4
Ammon Casey <acasey@panda-group.com>
2019-04-16 14:11:42 -1000

    version bump: 1.0.5

--- ptpkg.php
+++ ptpkg.php
@@ -16,7 +16,7 @@
  * Plugin Name:       PT Packages
  * Plugin URI:        https://github.com/pandatravel/wp-ptpkg
  * Description:       This plugin connects to panda's tour packages api
- * Version:           1.0.4
+ * Version:           1.0.5
  * Author:            Ammon Casey
  * Author URI:        https://github.com/ammonkc
  * License:           UNLICENSED
@@ -37,7 +37,7 @@ if (! defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('PTPKG_VERSION', '1.0.4');
+define('PTPKG_VERSION', '1.0.5');
 define('PTPKG_TEXTDOMAIN', 'ptpkg');
 define('PTPKG_NAME', 'ptpkg');
 define('PTPKG_URL', (WP_DEBUG ? 'https://ptpkg.test' : 'https://ptpkg.com'));
